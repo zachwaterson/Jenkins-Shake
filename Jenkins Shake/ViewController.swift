365e38e821fd1fd1f4c2b5f01bda3e2ce7061d57
trackstar192 <zwaterso@nd.edu>
2014-12-28 14:02:22 -0800

    before splash screen

--- Jenkins Shake/ViewController.swift
+++ Jenkins Shake/ViewController.swift
@@ -7,30 +7,95 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController, UIAlertViewDelegate, UINavigationControllerDelegate, UIPopoverControllerDelegate, UIImagePickerControllerDelegate {
-    @IBOutlet weak var imageView: UIImageView!
+class ViewController: UIViewController, UIAlertViewDelegate, UINavigationControllerDelegate, UIPopoverControllerDelegate, UIImagePickerControllerDelegate, UIGestureRecognizerDelegate {
+    //outlets
     @IBOutlet weak var choosePhotoButton: UIButton!
+    @IBOutlet weak var squareView: UIView!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var jenkinsImageView: UIImageView!
+    @IBOutlet weak var callToActionLabel: UILabel!
+    @IBOutlet weak var jenkinsLabel: UILabel!
     
-    
+    //properties
     var picker:UIImagePickerController?=UIImagePickerController()
     var popover:UIPopoverController? = nil
+    var lastScale = CGFloat(1.0)
+    var lastRotation = CGFloat(0.0)
+    var firstX = CGFloat(0.0)
+    var firstY = CGFloat(0.0)
+    var pastJenkinsNumbers = [-1, -2, -3]
+    var jenkinsSound:SystemSoundID = 0
     
     
+    //gesture stuff
+    @IBAction func handlePan(sender: UIPanGestureRecognizer) {
+        var translatedPoint = sender.translationInView(squareView)
+        if(sender.state == UIGestureRecognizerState.Began) {
+            firstX = jenkinsImageView.center.x
+            firstY = jenkinsImageView.center.y
+        }
+        
+        translatedPoint = CGPointMake(firstX + translatedPoint.x, firstY + translatedPoint.y)
+        
+        jenkinsImageView.center = translatedPoint
+        
+    }
+    
+    @IBAction func handlePinch(sender: UIPinchGestureRecognizer) {
+        if(sender.state == UIGestureRecognizerState.Began) {
+            lastScale = CGFloat(1.0)
+        }
+        
+        let scale = 1.0 - (lastScale - sender.scale)
+        
+        let currentTransform = jenkinsImageView.transform
+        let newTransform = CGAffineTransformScale(currentTransform, scale, scale)
+        jenkinsImageView.transform = newTransform
+        
+        lastScale = sender.scale
+        
+    }
+    
+    @IBAction func handleRotate(sender: UIRotationGestureRecognizer) {
+        if(sender.state == UIGestureRecognizerState.Ended) {
+            lastRotation = CGFloat(0.0)
+            return
+        }
+        
+        let rotation = 0.0 - (lastRotation - sender.rotation)
+        
+        let currentTransform = jenkinsImageView.transform
+        let newTransform = CGAffineTransformRotate(currentTransform, rotation)
+        jenkinsImageView.transform = newTransform
+        
+        lastRotation = sender.rotation
+                
+    }
+    
+    //buttons
+    @IBAction func didPressSaveButton(sender: UIButton) {
+        UIGraphicsBeginImageContext(squareView.frame.size)
+        squareView.layer.renderInContext(UIGraphicsGetCurrentContext())
+        let finalImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIImageWriteToSavedPhotosAlbum(finalImage, self, "image:didFinishSavingWithError:contextInfo:", nil)
+        
+    }
     
     @IBAction func didPressChoosePhotoButton(sender: AnyObject) {
-        var alert:UIAlertController=UIAlertController(title: "Choose Image", message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
+        let alert:UIAlertController=UIAlertController(title: "Choose Image", message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
         
-        var cameraAction = UIAlertAction(title: "Take Photo", style: UIAlertActionStyle.Default) {
+        let cameraAction = UIAlertAction(title: "Take Photo", style: UIAlertActionStyle.Default) {
             UIAlertAction in
             self.openCamera()
         }
         
-        var libraryAction = UIAlertAction(title: "Choose Existing", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
+        let libraryAction = UIAlertAction(title: "Choose Existing", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
             self.openLibrary()
         }
         
-        var cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (UIAlertAction) -> Void in
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (UIAlertAction) -> Void in
             //nothing
         }
         
@@ -48,9 +113,20 @@ class ViewController: UIViewController, UIAlertViewDelegate, UINavigationControl
         }
     }
     
+    //save feedback
+    func image(image: UIImage, didFinishSavingWithError error:NSErrorPointer, contextInfo: UnsafePointer<()>) {
+        let alert=UIAlertController(title: "Image Saved", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+        let cancelAction = UIAlertAction(title: "Great!", style: UIAlertActionStyle.Cancel) { (UIAlertAction) -> Void in
+            //nothing
+        }
+        alert.addAction(cancelAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
     func openCamera() {
         if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)) {
             picker!.sourceType = UIImagePickerControllerSourceType.Camera
+            picker!.allowsEditing = true
             self.presentViewController(picker!, animated: true, completion: nil)
         } else {
             self.openLibrary()
@@ -59,7 +135,6 @@ class ViewController: UIViewController, UIAlertViewDelegate, UINavigationControl
     
     func openLibrary() {
         picker!.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-        picker!.delegate = self
         picker!.allowsEditing = true
         if (UIDevice.currentDevice().userInterfaceIdiom == .Phone) {
             self.presentViewController(picker!, animated: true, completion: nil)
@@ -70,15 +145,77 @@ class ViewController: UIViewController, UIAlertViewDelegate, UINavigationControl
         }
     }
 
+    //after picking image
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
-        imageView.image = (info[UIImagePickerControllerEditedImage] as UIImage)
+        userImageView.image = (info[UIImagePickerControllerEditedImage] as UIImage)
         picker.dismissViewControllerAnimated(true, completion: nil)
+        callToActionLabel.hidden = true
+        changeJenkinsImage()
+        jenkinsImageView.hidden = false
     }
 
+    //allows multiple gestures at same time
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    
+    //allow for shake
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
+        if (motion == UIEventSubtype.MotionShake) {
+            changeJenkinsImage()
+        }
+    }
+    
+    func changeJenkinsImage() {
+        //make sure to not get the same jenkins photo
+        let numberOfJenkinsPhotos = 12
+        var newJenkinsNumber:Int = Int(rand())%numberOfJenkinsPhotos + 1
+        while ((contains(pastJenkinsNumbers, newJenkinsNumber))) {
+            newJenkinsNumber = Int(rand())%numberOfJenkinsPhotos + 1
+        }
+        let image = UIImage(named: "Jenkins" + String(newJenkinsNumber) + ".png")
+        //add to back of array and remove first element
+        pastJenkinsNumbers.append(newJenkinsNumber)
+        pastJenkinsNumbers.removeAtIndex(0)
+        jenkinsImageView.image = image!
+        //clear old transforms
+        jenkinsImageView.transform = CGAffineTransformIdentity
+        jenkinsImageView.sizeToFit()
+        jenkinsImageView.frame = CGRect(x: squareView.frame.width/4, y: squareView.frame.height/4, width: squareView.frame.width/2, height: squareView.frame.height/2)
+        
+        //jenkins animation
+        jenkinsLabel.alpha = 1.0
+        UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
+            self.jenkinsLabel.alpha = 0.0
+            }, completion: nil)
+        //jenkins noise
+        AudioServicesPlayAlertSound(jenkinsSound)
+        
+        }
+    
+    func getJenkinsSound() {
+        //load sound
+        let soundURL = NSBundle.mainBundle().URLForResource("jenkins", withExtension: "wav")
+        AudioServicesCreateSystemSoundID(soundURL, &jenkinsSound)
+        
+    }
+    
+    //for motion
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        jenkinsImageView.hidden = true
+        callToActionLabel.adjustsFontSizeToFitWidth = true
+        jenkinsLabel.alpha = 0.0
+        getJenkinsSound()
+        picker!.delegate = self
+
+        
     }
 
     override func didReceiveMemoryWarning() {
